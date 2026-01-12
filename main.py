0e39b276f049cd5eb730d9e5968c86dfac63e71f
Sebastian210102 <sebastian.rabadangranados@gmail.com>
2026-01-12 10:29:43 -0600

    Creacion objeto, empresa y proyecto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import sys 
 from services.excel_reader import leer_excel
+from core.empresa import Empresa
+from core.proyecto import Proyecto 
 
 INPUT_FOLDER = Path("input")
 REQUIRED_FILES = ["CONSTANCIA_SITUACION_FISCAL.pdf", "INE_T1.pdf", "INE_T2.pdf", "FOR_28.xlsx", 
@@ -8,7 +10,7 @@ REQUIRED_FILES = ["CONSTANCIA_SITUACION_FISCAL.pdf", "INE_T1.pdf", "INE_T2.pdf",
 RUTA_EXCEL = Path('input/FOR_28.xlsx')
 
 
-
+# Funciones encargadas de validación de los documentos y carpetas
 def verificacion_carpeta(directorio):
 
     if directorio.exists() and directorio.is_dir():
@@ -25,6 +27,8 @@ def verificacion_documento(documento):
         print(f'Documento "{documento}" no se encuentra')
         sys.exit(1)
 
+
+
 def main():
 
     verificacion_carpeta(INPUT_FOLDER)
@@ -35,6 +39,35 @@ def main():
     
     empresa_a_facturar = leer_excel(RUTA_EXCEL)
     print(f'Empresa a facturar : "{empresa_a_facturar}"')
+    
+    #Creando el objeto empresa
+
+    empresa = Empresa(
+        razon_social_propietario=None, 
+        razon_social_usuario= None, 
+        rfc= None,
+        actividad_economica=None,
+        domicilio= None,
+        telefono= None,
+        empresa_a_facturar=empresa_a_facturar
+        )
+    
+    #Creando objeto de el proyecto
+    proyecto = Proyecto(
+        id_proyecto= "1_D",
+        empresa= empresa,
+        numero_equipos=0,
+        estado="CREADO",
+        warnings= None
+        )
+    
+    # print(f'''Proyecto creado exitosamente:
+    #     ID : {proyecto.id_proyecto}
+    #     num equipos : {proyecto.numero_equipos}
+    #     empresa : {proyecto.empresa.empresa_a_facturar}
+    #     estado : {proyecto.estado}''')
+
+    
 
 if __name__ == "__main__":
     main()
