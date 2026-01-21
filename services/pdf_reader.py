fd0baf7275d767e510627088c12aecad75f561f0
Sebastian210102 <sebastian.rabadangranados@gmail.com>
2026-01-20 18:01:51 -0600

    Arreglo de la funcion lector de la constancia e implentación de la funcion normalizar nombre de la empresa

--- services/pdf_reader.py
+++ services/pdf_reader.py
@@ -2,6 +2,9 @@ import pdfplumber
 import sys
 from pathlib import Path
 
+RUTA_CONSTANCIA = Path('../input/CONSTANCIA_SITUACION_FISCAL.pdf')
+
+
 
 def leer_constancia_sat(ruta_pdf: Path) -> str:
     if not ruta_pdf.exists():
@@ -16,31 +19,47 @@ def leer_constancia_sat(ruta_pdf: Path) -> str:
             if texto:
                 texto_completo += texto + "\n"
 
-    # Normalizamos texto
-    texto_completo = texto_completo.replace("\n", " ")
 
+    texto_completo = texto_completo.replace("\n", "")
     razon_social = None
     regimen_capital = None
-
-    # Extracción simple (MVP)
-    if "Denominación/Razón Social:" in texto_completo:
-        razon_social = texto_completo.split(
-            "Denominación/Razón Social:"
-        )[1].split("Régimen")[0].strip()
-
-    if "Régimen Capital:" in texto_completo:
-        regimen_capital = texto_completo.split(
-            "Régimen Capital:"
-        )[1].split("Nombre Comercial")[0].strip()
-
+    if "Denominación/RazónSocial" in texto_completo : 
+        razon_social = texto_completo.split("Denominación/RazónSocial:"
+                                            )[1].split("RégimenCapital")[0].strip()
+    
+    if "RégimenCapital" in texto_completo:
+        regimen_capital = texto_completo.split("RégimenCapital:"
+                                               )[1].split("NombreComercial")[0].strip()
+    
     if not razon_social:
-        print("No se pudo obtener la Razón Social del SAT")
-        sys.exit(1)
+        print("No se pudo obtener razón social del SAT")
 
-    # Construcción del nombre final
+
+    #Contricción del nombre final 
     if regimen_capital:
         nombre_final = f"{razon_social}, {regimen_capital}"
     else:
         nombre_final = razon_social
 
-    return nombre_final
+    return razon_social, regimen_capital
+
+
+razon_social, regimen_capital = leer_constancia_sat(RUTA_CONSTANCIA)
+
+def normalizar_nombre_empresa(razon_social: str, regimen_raw: str) -> str:
+    # Separar palabras del nombre (heurística simple)
+    nombre = razon_social.upper()
+
+    # Mapeo explícito de regímenes
+    MAPEO_REGIMEN = {
+        "SOCIEDADDERESPONSABILIDADLIMITADADECAPITALVARIABLE": "S. DE R.L. DE C.V."
+    }
+
+    regimen = MAPEO_REGIMEN.get(regimen_raw, regimen_raw)
+
+    return f"{nombre}, {regimen}"
+
+
+
+
+print(normalizar_nombre_empresa(razon_social, regimen_capital))
